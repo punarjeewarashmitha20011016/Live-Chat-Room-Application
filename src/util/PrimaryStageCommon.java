415613b4f12ba1bdbd1830b301f8c5bd3db6d3d1
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-08-12 15:06:53 +0530

    removed structure

--- src/util/PrimaryStageCommon.java
+++ src/util/PrimaryStageCommon.java
@@ -1,6 +1,5 @@
 package util;
 
-import controllers.CreateCustomerFormController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -9,7 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class PrimaryStageCommon<T> {
-    public PrimaryStageCommon<CreateCustomerFormController> setStage(AnchorPane pane, T t, String title,String path) throws IOException {
+    public PrimaryStageCommon<T> setStage(AnchorPane pane, T t, String title, String path) throws IOException {
         Scene scene = new Scene(FXMLLoader.load(t.getClass().getResource(path)));
         Stage primaryStage = (Stage) pane.getScene().getWindow();
         primaryStage.setScene(scene);
