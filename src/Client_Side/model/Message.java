c3df2cc984baf5d495d90dcb256b9534796e2fc6
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-08-14 13:01:11 +0530

    added Message Encapsulated class

---
+++ src/Client_Side/model/Message.java
@@ -0,0 +1,38 @@
+package Client_Side.model;
+
+public class Message {
+    String name;
+    String message;
+
+    public Message() {
+    }
+
+    public Message(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "name='" + name + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
